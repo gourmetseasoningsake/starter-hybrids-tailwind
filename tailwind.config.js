1638ef4f912595abbef5d5e285913482af016c0a
gourmetseasoningsake <jerome@shtill.com>
2021-12-16 20:44:21 +0100

    creating

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,3 @@
-const fromRange =
-  (unit, step, max) =>
-  Object
-  .fromEntries(
-    [...Array(Math.floor(max/step)).keys()]
-    .map(x => [`${(x+1) * step}`, `${(x+1)}${unit}`])
-  )
-
-
-
-const fromArray =
-  (unit, arr) =>
-  Object
-  .fromEntries(
-    arr
-    .map(x => [`${x}`, `${x}${unit}`])
-  )
-
-  
-
 module.exports = {
   content: ['./public/**/*.html', './src/**/*.js'],
   theme: {},
